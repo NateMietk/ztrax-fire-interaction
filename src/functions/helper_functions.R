3957b6b8d63fc4b720604f4af8dbebb8e714a101
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-04-19 17:25:25 +0000

    add theme_pub

--- src/functions/helper_functions.R
+++ src/functions/helper_functions.R
@@ -13,21 +13,55 @@ load_data <- function(url, dir, layer, outname) {
   name
 }
 
-extract_one <- function(filename, shapefile_extractor) {
+extract_one <- function(filename, shapefile_extractor, prefix = prefix, s3_base = s3_base) {
   # function to extract all climate time series based on shapefile input
   # this results in large list of all months/years within the raster climate data
   # each list is written out to a csv so this only needs to be run once.
   # inputs:
   # filename -> a list of all tif filenames with full path
   # shapefile_extractor -> the shapefile (point or polygon) to extract climate data
-  
+
   out_name <- gsub('.tif', '.csv', filename)
   if (!file.exists(out_name)) {
-    res <- raster::cellStats(raster::stack(filename), shapefile_extractor,
+    res <- raster::extract(raster::raster(filename), shapefile_extractor,
                            na.rm = TRUE, stat = 'sum', df = TRUE)
     write.csv(res, file = out_name)
+    
+    system(paste0("aws s3 sync ",
+                  prefix, " ",
+                  s3_base))
   } else {
     res <- read.csv(out_name)
   }
   res
+}
+
+# GGPLOT Theme ------------------------------------------------------------
+theme_pub <- function(base_size=13, base_family="") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(hjust = 0.05, size = 13),
+            
+            panel.border = element_rect(colour = NA),
+            panel.background = element_rect(colour = NA),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            plot.background = element_rect(colour = NA),
+            
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            
+            legend.title = element_text(size=16),
+            legend.position = "right",
+            legend.text = element_text(size=16),
+            legend.direction = "vertical",
+            legend.key = element_rect(colour = "transparent", fill = "transparent"),
+            
+            strip.background=element_rect(colour=NA),
+            strip.text.x = element_text(size = 10),
+            
+            axis.title = element_text(size = 16),
+            axis.text.x = element_text(size = 16, angle = 65, hjust = 1),
+            axis.text.y = element_text(size = 16)))
 }
