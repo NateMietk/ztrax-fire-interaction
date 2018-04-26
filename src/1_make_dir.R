7468726a368c2ea192a011ef3e3bfb6bb92df078
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-04-26 13:46:26 -0600

    added cores

--- src/1_make_dir.R
+++ src/1_make_dir.R
@@ -6,6 +6,8 @@ lapply(x, library, character.only = TRUE, verbose = FALSE)
 proj_ea <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
 source('src/functions/helper_functions.R')
 
+cores <- detectCores()
+
 # Raw data folders
 prefix <- "data"
 raw_prefix <- file.path(prefix, "raw")
