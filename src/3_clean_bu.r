b75dc53a63ef2e99e9bcc90914fad8512ad73af0
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-16 08:39:41 -0600

    parallel extract - using snow

--- src/3_clean_bu.r
+++ src/3_clean_bu.r
@@ -78,15 +78,16 @@ if (!exists('sum_extractions_bu')) {
 if (!exists('sum_fpa_bu')) {
   if (!file.exists(file.path(anthro_out, 'building_counts', 'building_counts_all', 'summary_fpa_bu.rds'))) {
 
+    library(snow)
+    beginCluster(n = 2)
+
     sum_fpa_bu <-
-      lapply(
-        as.list(bu_list),
-        FUN = extract_one,
-        shapefile_extractor = fpa,
-        funny = sum,
-        use_varname = TRUE,
-        varname = 'fpa_sum_'
-      )
+      raster::extract(bu_stack,
+                      fpa,
+                      na.rm = TRUE,
+                      stat = 'sum',
+                      df = TRUE)
+    endCluster()
 
     sum_fpa_bu_count <- sum_fpa_bu %>%
       bind_cols %>%
